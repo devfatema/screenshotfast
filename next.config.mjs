151e1dba95ad94aa6df07ada7c15102dfff1b9b2
Arafat Hossain <harafat949@gmail.com>
2024-10-04 22:47:15 +0600

    Issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    images: {
+        domains: ['screenshotfast.lemonsqueezy.com'],
+    },
+};
 
 export default nextConfig;
