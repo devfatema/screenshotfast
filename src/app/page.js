e730fb856378cc3d76fd3f5590665d86dd1114a7
Arafat Hossain <harafat949@gmail.com>
2024-10-04 23:10:47 +0600

    Some Issue Fix

--- src/app/page.js
+++ src/app/page.js
@@ -195,7 +195,7 @@ export default function Home() {
                   <span>✨ Pre-launch Offer</span>
                 </div>
                 <div>
-                  <p className=" text-[13px]">Limited time offers 15% discount using "<strong>EARLY15</strong>" coupon code</p>
+                  <span className=" text-[13px]">Limited time offers 15% discount using "<strong>EARLY15</strong>" coupon code</span>
                 </div>
                 <div><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 50 50" className="mx-auto"><path fill="currentColor" d="M25 42c-9.4 0-17-7.6-17-17S15.6 8 25 8s17 7.6 17 17s-7.6 17-17 17m0-32c-8.3 0-15 6.7-15 15s6.7 15 15 15s15-6.7 15-15s-6.7-15-15-15" /><path fill="currentColor" d="m24.7 34.7l-1.4-1.4l8.3-8.3l-8.3-8.3l1.4-1.4l9.7 9.7z" /><path fill="currentColor" d="M16 24h17v2H16z" /></svg></div>
               </Link>
